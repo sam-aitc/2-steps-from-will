d7c0e9e2541cca01aa4c0b01e35b47658f9c4708
sam <sam@aitc.com.tw>
2022-08-03 04:52:14 -0400

    待解：目前 docker logs 無查看到日誌

---
+++ demo001.dockerfile
@@ -0,0 +1,12 @@
+FROM samaitc/gcc11 AS builder
+WORKDIR /demo/bin
+WORKDIR /demo/src
+COPY main.c /demo/src/
+RUN git clone https://github.com/mattbudish/libmu.git ../libmu && gcc -o ../bin/demoServer main.c ../libmu/src/mu.c -I../libmu/src -luv -lmicrohttpd
+
+FROM samaitc/ubuntu2204
+COPY --from=builder /demo/bin/demoServer /demo/
+# ENTRYPOINT [  ]
+CMD [ "/demo/demoServer" ]
+
+# docker image build -f demo001.dockerfile -t samaitc/demo001 .
---
+++ main.c
@@ -0,0 +1,42 @@
+#include "mu.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+const int PORT = 3000;
+
+unsigned int say_hello(const MU_REQUEST *req, MU_RESPONSE *res);
+
+unsigned int echo(const MU_REQUEST *req, MU_RESPONSE *res);
+
+void listen_cb();
+
+int main(void)
+{
+    mu_get("/", say_hello);
+
+    mu_post("/", echo);
+
+    return mu_listen(PORT, listen_cb);
+}
+
+unsigned int say_hello(const MU_REQUEST *req, MU_RESPONSE *res)
+{
+    res->body = strdup("Hello World!");
+    res->body_size = strlen(res->body);
+    return 200;
+}
+
+unsigned int echo(const MU_REQUEST *req, MU_RESPONSE *res)
+{
+    res->body = malloc(req->body_size + 2);
+    sprintf(res->body, "%.*s\n", req->body_size, req->body);
+    res->body_size = strlen(res->body);
+    return 200;
+}
+
+void listen_cb()
+{
+    printf("libmu server listening on port %d\n", PORT);
+    fflush(stdout);
+}
